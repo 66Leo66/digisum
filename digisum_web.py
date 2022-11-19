d2d700ac7f669da591530941bd34b7c15cab68de
66Leo66 <33322229+66Leo66@users.noreply.github.com>
2022-11-19 20:28:27 +0800

    all done!

---
+++ digisum_web.py
@@ -0,0 +1,118 @@
+from pywebio.input import input_group, input, select, checkbox, NUMBER
+from pywebio.output import (
+    put_markdown,
+    use_scope,
+    put_processbar,
+    set_processbar,
+    put_button,
+    put_file,
+    put_text,
+    put_loading,
+)
+from pywebio.platform.tornado import start_server
+from pywebio_battery import put_logbox, logbox_append, confirm
+from libdigisum import Step, solve_max, solve_min, expected_answer, digisum
+import random
+
+MAX_NUMBER = 1e9 + 5
+
+
+def validate_n(n):
+    if n < 1:
+        return "需要一个正整数"
+    elif n >= MAX_NUMBER:
+        return "数字太大了，请在本地部署并计算"
+    return None
+
+
+def logbox_callback(prog: float, step: Step):
+    logbox_append("out", step.to_string() + "\n")
+
+
+def progress_callback(prog: float, step: Step):
+    set_processbar("progress", prog)
+
+
+def get_file(res: dict):
+    with put_loading():
+        alphabet = [chr(i) for i in range(ord("a"), ord("z") + 1)]
+        random.shuffle(alphabet)
+        rand = "".join(random.sample(alphabet, 6))
+        msg = ""
+        for st in res["steps"]:
+            msg += st.to_string()
+            msg += "\n"
+        ctnt = msg.encode("utf-8")
+        put_file(name=f"result-{rand}.txt", content=ctnt, scope="result")
+
+
+def digisum_io():
+    put_markdown("# 数位和求解程序")
+    user_input = input_group(
+        "运行参数",
+        [
+            input(
+                label="输入参数 n", name="num", type=NUMBER, validate=validate_n, value=2018
+            ),
+            select(
+                label="选择求解类型",
+                name="type",
+                options=[
+                    {"label": "最大值", "value": "max"},
+                    {"label": "最小值", "value": "min"},
+                ],
+            ),
+            checkbox(
+                label="额外参数",
+                name="extras",
+                options=[{"label": "实时显示结果", "value": "live_output"}],
+            ),
+        ],
+    )
+    display_msg: str
+    with use_scope("precalc"):
+        inp = user_input["num"]
+        exp_max = expected_answer(int(inp))
+        exp_min = exp_max
+        while exp_min > 10:
+            exp_min = digisum(exp_min)
+
+        display_msg = f"根据您的输入数据 n={inp}:\n结果的最小值为 {exp_min}\n结果的最大值为 {exp_max}"
+        ask = confirm(
+            title="计算已初步完成",
+            content=[
+                display_msg,
+                put_markdown("### 您要继续求解运算步骤吗"),
+            ],
+        )
+        with use_scope("result"):
+            if ask:
+                # print(int(user_input["num"]))
+                callback: object
+                if "live_output" in user_input["extras"]:
+                    put_logbox("out")
+                    callback = logbox_callback
+                else:
+                    put_processbar("progress")
+                    callback = progress_callback
+
+                res: dict
+                if user_input["type"] == "max":
+                    res = solve_max(int(user_input["num"]), callback)
+                else:
+                    res = solve_min(int(user_input["num"]), callback)
+                put_markdown(f"### 解得答案为: {res['answer']}")
+                put_button(label="将步骤保存为文本文件", onclick=lambda: get_file(res))
+            else:
+                put_text(display_msg)
+                put_markdown("### 感谢使用")
+
+
+if __name__ == "__main__":
+    # digisum_io()
+    start_server(
+        digisum_io,
+        port=0,
+        host=None,
+        remote_access=True,
+    )
--- libdigisum.py
+++ libdigisum.py
@@ -18,7 +18,7 @@ def expected_answer(n: int) -> int:
     if n < 10:
         return n
     # k = sum(range(1, n + 1))
-    k = (n + 1) * n / 2
+    k = int((n + 1) * n / 2)
     while k >= 10:
         k = digisum(k)
     numlen = len(str(n)) - 1
@@ -37,25 +37,31 @@ def expected_answer(n: int) -> int:
     return 1
 
 
-def solve_min(n: int) -> dict:
+def solve_min(n: int, callback) -> dict:
     steps = []
     nums = list(range(0, n + 1))
     for i in range(n - 1, 1, -1):
         steps.append(Step(nums[i + 1], nums[i]))
+        callback(len(steps) / (n - 1), steps[-1])
+
         nums[i] = steps[-1].dsum
+
     steps.append(Step(nums[2], nums[1]))
+    callback(len(steps) / (n - 1), steps[-1])
     return {"answer": steps[-1].dsum, "steps": steps}
 
 
-def sequential_merge(nums: list, l: int, r: int) -> list:
-    steps = []
+def sequential_merge(n: int, steps: list, nums: list, l: int, r: int, callback) -> list:
+    # steps = []
     for i in range(l + 1, r + 1):
         steps.append(Step(nums[i - 1], nums[i]))
+        callback(len(steps) / (n - 1), steps[-1])
         nums[i] = steps[-1].dsum
-    return steps
+    # return steps
 
 
-def solve_max(n: int, exp_ans: int) -> dict:
+def solve_max(n: int, callback) -> dict:
+    exp_ans = expected_answer(n)
     steps = []
     nums = list(range(0, n + 1))
 
@@ -76,14 +82,21 @@ def solve_max(n: int, exp_ans: int) -> dict:
     else:
         mid = exp_ans
 
-    steps.extend(sequential_merge(nums, 1, mid - 1))
-    steps.extend(sequential_merge(nums, mid + 1, n))
+    # steps.extend(sequential_merge(n, nums, 1, mid - 1, callback))
+    # steps.extend(sequential_merge(n, nums, mid + 1, n, callback))
+    sequential_merge(n, steps, nums, 1, mid - 1, callback)
+    sequential_merge(n, steps, nums, mid + 1, n, callback)
 
     if 1 < mid and mid < n:
         steps.append(Step(nums[mid - 1], nums[n]))
+        callback(len(steps) / (n - 1), steps[-1])
+
         steps.append(Step(steps[-1].dsum, mid))
+        callback(len(steps) / (n - 1), steps[-1])
     else:
         op = nums[n] if mid == 1 else nums[n - 1]
+
         steps.append(Step(op, mid))
+        callback(len(steps) / (n - 1), steps[-1])
 
     return {"answer": steps[-1].dsum, "steps": steps}
